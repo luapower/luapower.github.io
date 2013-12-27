190604fdd7abb21845e9466269e2aa941eac29ed
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2013-12-28 00:17:11 +0200

    update

--- update-deps.sh
+++ update-deps.sh
@@ -5,6 +5,7 @@ for s in *.md; do
 	s="${s%.*}"  # strip extension
 	[ -f "$s.lua" ] && grep "requires:" "$s.md" >/dev/null && {
 		deps="$(cd _docs; sh ./dependencies.sh "$s")"
+		sed -i "s/ \| requires\: /d" "$s.md"
 		#sed -i "s/ \| requires\:.*/ \| requires\: $deps/" "$s.md"
 	}
 done
